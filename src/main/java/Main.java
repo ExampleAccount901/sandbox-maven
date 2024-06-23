4e3217fa78473cd7862146887750869a14348938
AndrewKozyrev <mr.andrey.kozyrev@gmail.com>
2024-06-24 00:55:36 +0300

    conditions task

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,19 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        String line = new Scanner(System.in).nextLine();
 
+        if (isPhoneNumber(line)) {
+            System.out.println("String is a phone number");
+        } else {
+            System.out.println("String is not a phone number");
+        }
+    }
+
+    private static boolean isPhoneNumber(String str) {
+
+
+        return false;
     }
 }
